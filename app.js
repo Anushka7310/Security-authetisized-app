646da58d70af59516b060a02765b92cb9c722dd5
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2022-02-06 12:34:08 +0530

    final commit

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  secret: String,
 });
 
 //to hash and save our passwords and to save it in mongoose database/////
@@ -99,11 +101,39 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+  User.find({"secret":{$ne: null}}, function(err, foundUsers){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers){
+        res.render("secrets", {userWithSecrets: foundUsers});
+      }
+    }
+  })
+});
+
+app.get("/submit", function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secret");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function (req, res) {
